7a5af83c439f963db6cb43702697fe5e7c5859b3
Simran Sheth <simransheth@Simrans-MacBook-Air.local>
2018-11-08 18:13:00 -0500

    update 8/11

--- code/app/src/main/java/com/example/simransheth/seniorproject/MainActivity.java
+++ code/app/src/main/java/com/example/simransheth/seniorproject/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.simransheth.seniorproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
+import com.google.firebase.*;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     private TextView header;
     private EditText emailID;
     private EditText password;
+    private TextView messageDisplay;
     private Button loginBtn;
     private Button forgotPasswordBtn;
     private Button newUserBtn;
@@ -18,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        header = findViewById(R.id.header);
+        emailID = findViewById(R.id.emailID);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+        forgotPasswordBtn = findViewById(R.id.forgotPasswordBtn);
+        newUserBtn = findViewById(R.id.newUserBtn);
+        messageDisplay = findViewById(R.id.messageDisplay);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageDisplay.setText("Button Works");
+            }
+        });
     }
 
 
